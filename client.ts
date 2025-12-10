959b49417ecda2d9a6bd50a3825dbd8019c5d0b4
Calegree <c.iturra07@ufromail.cl>
2025-12-10 05:39:03 -0300

    fix: fixing autorization

--- client.ts
+++ client.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import { Platform } from "react-native";
 
-const LOCAL_IP = "192.168.1.85";
+const LOCAL_IP = "192.168.1.87";
 const PORT = 8080;
 
 const baseURL =
   Platform.OS === "web"
     ? `http://localhost:${PORT}/api`
-    : `http://${LOCAL_IP}:${PORT}/api`;
-
+  : `http://${LOCAL_IP}:${PORT}/api`;
+   // : `http://localhost:${PORT}/api`;
 console.log("🌐 API BASE URL:", baseURL);
 
 const api = axios.create({
